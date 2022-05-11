43556cef227edd3c34a81061e49f0ea29e2b021d
nurmdrafi <nurmdrafi2014@gmail.com>
2022-05-12 02:53:08 +0600

    config custom theme and setup react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import Navbar from "./Pages/Shared/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,30 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0FCFEC",
+
+          secondary: "#19D3AE",
+
+          accent: "#3A4256",
+
+          neutral: "#191D24",
+
+          "base-100": "#F6F7F9",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
-}
+};
