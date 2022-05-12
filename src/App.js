376b169454c9f30c07e0754f724f965fc38207e7
nurmdrafi <nurmdrafi2014@gmail.com>
2022-05-12 16:16:30 +0600

    Navbar and component created

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import Navbar from "./Pages/Shared/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
+import Appointment from "./Pages/Appointment/Appointment";
+import Reviews from "./Pages/Reviews/Reviews";
+import Contact from "./Pages/Contact/Contact";
+import Login from "./Pages/Login/Login";
+import NotFound from "./Pages/NotFound/NotFound";
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </div>
   );
